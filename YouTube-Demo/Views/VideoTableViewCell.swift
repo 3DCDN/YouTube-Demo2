a3a6b1c3250b368c7255f575230fd24a9f305429
3DCDN <richst.ongeywg@gmail.com>
2022-01-17 12:50:27 -0600

    Rearranged files into groups. Added gitignore file

---
+++ YouTube-Demo/Views/VideoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoTableViewCell.swift
+//  YouTube-Demo
+//
+//  Created by Richard St. Onge on 2022-01-17.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
